f958c2e0dcaa38f6c241cf851086fa4cdc0aa9ad
Nikola Tasic <ntasic7@gmail.com>
2020-10-07 19:30:16 +0200

    added node-sass, added boilerplate meta tags

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 import "materialize-css/dist/css/materialize.min.css";
 import "../node_modules/animate.css/animate.min.css";
-import "./assets/styles/materialize-css-override.css";
-import "./assets/styles/index.css";
+import "./assets/styles/materialize-css-override.scss";
+import "./assets/styles/index.scss";
 
 ReactDOM.render(
 	<React.StrictMode>
